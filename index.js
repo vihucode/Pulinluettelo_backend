abe29fae54f5e368b62ca1cb6eb315d6acd3459e
Vili Huotari <vili.huotari@gmail.com>
2023-01-26 17:00:52 +0200

    Deleted morgan

--- index.js
+++ index.js
@@ -1,12 +1,9 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
 
 app.use(express.json())
 app.use(express.static('build'))
-var morgan = require('morgan')
-
-const cors = require('cors')
-
 app.use(cors())
 
 let persons = [
